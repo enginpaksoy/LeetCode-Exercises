6a110c6290b306784b4ecd2f99e7e25afb4a37cb
Engin Paksoy <96542214+enginpaksoy@users.noreply.github.com>
2024-03-12 00:15:31 +0300

    Update 9. Palindrome Number.c

--- 9. Palindrome Number.c
+++ 9. Palindrome Number.c
@@ -1,5 +1,8 @@
 bool isPalindrome(int x) {
-    int y = 0;
+    if(x < 0){
+        return false;
+    }
+    long int y = 0;
     for(int i = x; i > 0; i /= 10){
         y *= 10;
         int a = i % 10;
